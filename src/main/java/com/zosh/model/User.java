5d67530023df9763f4e74bb1350a554a83a1c023
Leena <panchalleena8839@gmail.com>
2024-03-15 12:51:04 +0530

    Project first commit

---
+++ src/main/java/com/zosh/model/User.java
@@ -0,0 +1,28 @@
+package com.zosh.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+
+
+}
