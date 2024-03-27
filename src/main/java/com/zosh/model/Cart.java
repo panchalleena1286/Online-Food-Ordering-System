133b84b372dbde015facc50c2584a0f41bc5f54a
Leena <panchalleena8839@gmail.com>
2024-03-27 22:25:02 +0530

    Project commit

--- src/main/java/com/zosh/model/Cart.java
+++ src/main/java/com/zosh/model/Cart.java
@@ -1,12 +1,12 @@
 package com.zosh.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -17,5 +17,13 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @OneToOne
+    private User customer;
+
+    private Long total;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> item = new ArrayList<>();
 
 }
