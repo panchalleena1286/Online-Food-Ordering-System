381f1ba036f7deaf49df30834a31de96f403b56d
Leena <panchalleena8839@gmail.com>
2024-03-29 22:32:45 +0530

    Project commit

---
+++ src/main/java/com/zosh/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.zosh.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "nrnreofwjffsporgcdhcpsknwfjowhfejnfjelfiefhkenfeofjhbfciekkfelnkfin";
+    public static final String JWT_HEADER = "Authorization";
+}
