f17fb5818dc4759168a863bb3ea115834f21d349
Leena <panchalleena8839@gmail.com>
2024-03-31 10:53:27 +0530

    Project commit

---
+++ src/main/java/com/zosh/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.zosh.repository;
+
+import com.zosh.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
