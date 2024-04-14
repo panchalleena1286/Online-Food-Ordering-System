954458aefffb14a3088ad9ad004b9d24019c47e0
Leena <panchalleena8839@gmail.com>
2024-04-14 18:23:27 +0530

    create Restaurant Service

--- src/main/java/com/zosh/service/RestaurantService.java
+++ src/main/java/com/zosh/service/RestaurantService.java
@@ -1,10 +1,29 @@
 package com.zosh.service;
 
+import com.zosh.dto.RestaurantDto;
 import com.zosh.model.Restaurant;
 import com.zosh.model.User;
 import com.zosh.request.CreateRestaurantRequest;
 
+import java.util.List;
+
 public interface RestaurantService {
 
     public Restaurant cretaeRestaurant(CreateRestaurantRequest req, User user);
+
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updatedRestaurant) throws Exception;
+
+    public Restaurant deleteRestaurant(Long restaurantId) throws Exception;
+
+    public List<Restaurant> getAllRestaurant();
+
+    public List<Restaurant> searchRestaurant();
+
+    public Restaurant findRestaurantById(Long id) throws Exception;
+
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception;
+
+    public RestaurantDto addToFavorites(Long restaurantId, User user) throws Exception;
+
+    public Restaurant updateRestaurantStatus(Long id) throws Exception;
 }
