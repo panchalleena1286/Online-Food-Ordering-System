3ae599d3869828b681b241151a2c51a62423ff16
Leena <panchalleena8839@gmail.com>
2025-06-16 22:07:13 +0530

    Initial commit: Online Food Ordering System backend

---
+++ src/main/java/com/foodapp/backend/FoodorderingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorderingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodorderingBackendApplication.class, args);
+	}
+
+}
